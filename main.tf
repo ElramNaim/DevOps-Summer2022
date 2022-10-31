3dc46e098c14bd22759c287094e76a3320ab4e0c
Elram <Elram.naim@gmail.com>
2022-10-31 13:55:40 +0200

    Upload

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+#####################################################################################
+# Create a VPC
+resource "aws_vpc" "testing" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "elram-naim-dev-vpc"
+  }
+}
+resource "aws_subnet" "Subnet_Web" {
+  vpc_id     = aws_vpc.testing.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "elram-naim-k8s-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.testing.id
+
+  tags = {
+    Name = "IGW"
+  }
+}
+
+resource "aws_route" "routeIGW" {
+  route_table_id         = aws_vpc.testing.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+
